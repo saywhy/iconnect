53ba43da6b3470dd3b755691c6b5c49a4492faf2
zhongshuaisun <zhongshuaisun@126.com>
2019-08-27 11:44:28 +0800

    更改时区选择时间框加长

--- src/frontend/views/seting/systime.php
+++ src/frontend/views/seting/systime.php
@@ -1,28 +1,25 @@
 <?php
-use yii\helpers\Url;
-use yii\grid\GridView;
-use yii\widgets\LinkPager;
 /* @var $this yii\web\View */
 
 $this->title = '系统时间';
 ?>
   <link rel="stylesheet" href="/css/systime.css">
 <!-- Main content -->
-<section class="content" ng-app="myApp" ng-controller="systime" >
+<section class="content" ng-app="myApp" ng-controller="systime" ng-cloak>
 
 <div class="row">
     <div class="col-xs-12">
       <div class="nav-tabs-custom">
 
         <?php include 'nav.php';?>
-        
+
         <div class="tab-content" ng-cloak>
 
           <!-- systime-->
         <div class="head_name">
                 <span class="head_mid_title">配置方式:</span>
                 <label>
-                    <select class="form-control input_style" 
+                    <select class="form-control input_style"
                     ng-change = "typeChange()"
                     ng-model="selectedName"  ng-options="x.value as x.name for x in datatype"></select>
                 </label>
@@ -30,7 +27,7 @@ $this->title = '系统时间';
          <div class="head_name">
                 <span class="head_mid_title">日期选择:</span>
                 <label>
-                   <input type="text" ng-click="custom()" class="form-control input_style" id="reservationtime" readonly 
+                   <input type="text" ng-click="custom()" class="form-control input_style" id="reservationtime" readonly
                    ng-disabled="timedisabled">
                 </label>
         </div>
@@ -39,12 +36,12 @@ $this->title = '系统时间';
                 <label>
                 <select-search  zonedisabled = 'zonedisabled' searchvalue ='zoneresult' datas="datas"></select-search>
                 </label>
-               
+
         </div>
          <div class="head_name">
                 <span class="head_mid_title">NTP时间:</span>
                 <label>
-                    <input class="form-control input_style" type="text" ng-model="ntpTime" 
+                    <input class="form-control input_style" type="text" ng-model="ntpTime"
                     ng-disabled="ntpdisabled">
                 </label>
         </div>
@@ -63,7 +60,7 @@ $this->title = '系统时间';
 </div>
 </section>
 
-           
+
 
 <script type="text/javascript" src="/js/controllers/systime.js"></script>
 
